b8d913bf95f47ca2c321578f175206d31acfd3fa
thoriqr <87070852+thoriqr@users.noreply.github.com>
2021-08-05 00:30:23 +0700

    membuat pages dan component todo

--- src/App.js
+++ src/App.js
@@ -1,56 +1,8 @@
 import React from "react";
 import "./styles.css";
 
-import Paper from "./components/Paper";
+import TodoList from "./pages/TodoList";
 
 export default function App() {
-  return (
-    <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">Todo Lists</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-      </section>
-    </Paper>
-  );
+  return <TodoList />;
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import Todo from "./Todo";
+
+const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props di React"
+    },
+    {
+      text: "Belajar Hooks"
+    },
+    {
+      text: "Belajar state di React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar props di React"
+    },
+    {
+      text: "Belajar Hooks"
+    },
+    {
+      text: "Belajar state di React"
+    },
+    {
+      text: "Belajar React"
+    }
+  ];
+
+  return (
+    <section className="todos">
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
+    </section>
+  );
+};
+
+export default Todos;
