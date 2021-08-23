dc774cdafc63f21e802ffcd543cb36fc92833767
Thoriq Rahman <87070852+thoriqr@users.noreply.github.com>
2021-08-23 14:01:59 +0700

    emotion css props di Button components

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./todos.module.css";
 
-import Todo from "./Todo";
+import Todo from "../todo/Todo";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className="todos">
+    <section className={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -19,10 +20,10 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className="todo-placeholder-text">
+        <div className={styles.todoPlaceHolderText}>
           add todo by clicking{" "}
-          <span className="add-button-placeholder-text">Add</span> button on the
-          top left corner
+          <span className={styles.addButtonPlaceHolderText}>Add</span> button on
+          the top left corner
         </div>
       )}
     </section>
